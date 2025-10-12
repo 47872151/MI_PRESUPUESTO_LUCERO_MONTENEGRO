95087368837d9718e589c29c83f30a0499c46d4c
María Eugenia Lucero <48313612@est.ort.edu.ar>
2025-10-12 13:48:25 -0300

    vista listado creada

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout";
-import Listado from "./pages/Listado";
+import Listado from "./pages/Listado"; 
 import Nuevo from "./pages/Nuevo";
 import Editar from "./pages/Editar";
 import Resumen from "./pages/Resumen";
@@ -12,9 +12,9 @@ function App() {
       <Route path="/" element={<Layout />}>
         <Route index element={<Listado />} />
         <Route path="nuevo" element={<Nuevo />} />
-        <Route path="editar/:id" element={<Editar />} />
         <Route path="resumen" element={<Resumen />} />
         <Route path="ajustes" element={<Ajustes />} />
+        <Route path="editar/:id" element={<Editar />} />
       </Route>
     </Routes>
   );
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -5,11 +5,11 @@ import { Outlet } from "react-router-dom";
 function Layout() {
   return (
     <div className="layout">
-      <Header />
-      <main className="contenido">
-        <Outlet />
-      </main>
       <Navbar />
+      <div className="content">
+        <Header />
+        <Outlet />
+      </div>
     </div>
   );
 }
